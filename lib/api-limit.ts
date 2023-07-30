3ab295a1ce3d51e525b1054c578c0e6971f37882
Jevon Cochran <jevon@securebags.com>
2023-07-29 20:22:47 -0500

    incrementGenerationCount

---
+++ lib/api-limit.ts
@@ -0,0 +1,24 @@
+import { auth } from "@clerk/nextjs";
+import prismadb from "./prismadb";
+import { FREE_GENERATIONS } from "@/constants";
+
+export const incrementGenerationCount = async () => {
+  const { userId } = auth();
+
+  if (!userId) return;
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: { userId, count: 1 },
+    });
+  }
+};
