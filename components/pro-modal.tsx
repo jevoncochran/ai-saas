4371834ac196b2cd2ede8d4e234755ea43fc4b85
Jevon Cochran <jevon@securebags.com>
2023-07-30 23:21:04 -0500

    stripe subscription endpoint

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -15,9 +16,24 @@ import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { Check, Zap } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 
 const ProModal = () => {
   const proModal = useProModal();
+
+  const [loading, setLoading] = useState(false);
+
+  const onSubscripe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+      window.location.href = response.data.url;
+    } catch (error: any) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
@@ -48,7 +64,12 @@ const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            size="lg"
+            variant="premium"
+            className="w-full"
+            onClick={onSubscripe}
+          >
             Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
         </DialogFooter>
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,3 +4,5 @@ export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2022-11-15",
   typescript: true,
 });
+
+
